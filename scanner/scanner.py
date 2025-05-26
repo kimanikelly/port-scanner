c2b7b1499a2053cf4c1ba945854dbd6e4a88db95
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2025-05-26 14:55:27 -0400

    Integrating the global function that checks for open ports Defined the client_connection function with an ip_address & port parameter Defined the for loop that iterates between the provided ip_address tuple Defined a nested if statement that checks if the port is open

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -2,4 +2,14 @@ import socket
 from socket import AF_INET
 from socket import SOCK_STREAM
 
-socket_instance = socket.socket(AF_INET,SOCK_STREAM)
+def client_connection(ip_address=str,port=tuple[int]): 
+
+    for port in range(port[0],port[1]):
+
+        socket_instance = socket.socket(AF_INET,SOCK_STREAM)
+
+        if socket_instance.connect_ex((ip_address,port)) == 0:
+            print(port)
+        
+client_connection("127.0.0.1",(1,10000))
+
